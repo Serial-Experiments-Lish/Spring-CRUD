4be8e8952210cb618ebf64bae9f96527bdbb3879
ShengtraneChooChoo <lishengashiluli@gmail.com>
2022-06-07 06:58:06 -0400

    Initials

---
+++ src/main/java/com/sprudleaf/demo/SprudleafApplication.java
@@ -0,0 +1,13 @@
+package com.sprudleaf.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprudleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprudleafApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sprudleaf/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.sprudleaf.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sprudleaf.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    
+}
